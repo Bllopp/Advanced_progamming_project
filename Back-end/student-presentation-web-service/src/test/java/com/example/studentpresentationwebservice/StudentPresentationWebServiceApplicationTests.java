7d9de5a05e05514fca435a82a79f1bfef266aba7
Bllopp <pablo.ratouit@efrei.net>
2024-01-11 12:41:04 +0100

    initial commit for stuudent presentation service

---
+++ Back-end/student-presentation-web-service/src/test/java/com/example/studentpresentationwebservice/StudentPresentationWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentpresentationwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPresentationWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
