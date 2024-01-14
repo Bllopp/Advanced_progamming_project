2f5b9db0848ba260c4fcf6bf3bd44ec824936cc5
Pablo <pablo.ratouit@efrei.net>
2024-01-14 17:00:07 +0100

    script fix

--- Database/script.sql
+++ Database/script.sql
@@ -6,18 +6,18 @@ CREATE DATABASE IF NOT EXISTS `user_APP`;
 
 USE `user_APP`;
 
-CREATE TABLE IF NOT EXISTS `user` (`userId`  int NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, `mail` VARCHAR(320) NOT NULL, `password` NOT NULL VARCHAR(30), PRIMARY KEY(`ID`))ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS `user` (`userId` int NOT NULL AUTO_INCREMENT, `name` VARCHAR(100) NOT NULL, `mail` VARCHAR(320) NOT NULL, `password` VARCHAR(30) NOT NULL , PRIMARY KEY(`userId`))ENGINE=INNODB;
 
-CREATE TABLE IF NOT EXISTS `role` (`userId` int not null, `role` varchar(50) not null, PRIMARY KEY (`userId`), constraint `FK_userId` FOREIGN KEY (`userId`) references `user`(`userId`)) ENGINE=INNODB;
+CREATE TABLE IF NOT EXISTS `role` (`userId` int not null, `role` varchar(50) not null, PRIMARY KEY (`userId`), constraint `FK_userId` FOREIGN KEY (`userId`) references `user`(`userId`))ENGINE=INNODB;
 
 -- For presentation database which contains presentations informations and the dates proposed by the student
 
 CREATE DATABASE IF NOT EXISTS `presentation_APP`;
 USE `presentation_APP`;
 
-create table if not exists `presentation` (`presId` not null int auto_increment, `studentId` not null int, `mode` not null varchar(30), `teacherId` not null int, `tutorId` not null int, `finalDate` date, primary key (`id`), constraint `FK_teacherId` foreign key (`teacher`) references `user_APP`.`user`(`userId`), constraint `FK_tutorId` foreign key (`tutorId`) references `user_APP`,`user`(`userId`)) ENGINE=INNODB;
+create table if not exists `presentation` (`presId` int not null auto_increment, `studentId` int not null, `mode` varchar(30) not null , `teacherId` int not null , `tutorId` int not null , `finalDate` date, primary key (`presId`), constraint `FK_teacherId` foreign key (`teacherId`) references `user_APP`.`user`(`userId`), constraint `FK_tutorId` foreign key (`tutorId`) references `user_APP`.`user`(`userId`))ENGINE=INNODB;
 
-create table if not exists `presentationDates` (`presId` not null int, `date` date not null, `teacherVote` int, `tutorVote` int, primary key(`presId`,`date`), constraint `FK_presId` foreign key (`presId`) references `presentation`(`presId`) ) ENGINE=INNODB; 
+create table if not exists `presentationDates` (`presId` int not null , `date` date not null, `teacherVote` int, `tutorVote` int, primary key(`presId`,`date`), constraint `FK_presId` foreign key (`presId`) references `presentation`(`presId`))ENGINE=INNODB; 
 
 
 -- For report database which contains report informations the validation of it 
@@ -25,4 +25,4 @@ CREATE DATABASE IF NOT EXISTS `report_APP`;
 
 USE `report_APP`;
 
-create table if not exists `report` (`studentId` not null int, primary key (`studentId`), `file` MEDIUMBLOB not null, teacherId not null int, constraint `FK_report_teacherId` foreign key (`teacherId`) references `user_APP`.`user`(`userId`), `teacherVote` int , `tutorId` not null int, constraint `FK_report_tutorId` foreign key (`tutorId`) references `user_APP`.`user`(`id`), `tutorVote` int, `uploadDate` not null date )ENGINE=INNODB;
+create table if not exists `report` (`studentId` int not null , primary key (`studentId`), `file` MEDIUMBLOB not null, teacherId int not null , constraint `FK_report_teacherId` foreign key (`teacherId`) references `user_APP`.`user`(`userId`), `teacherVote` int , `tutorId` int not null, constraint `FK_report_tutorId` foreign key (`tutorId`) references `user_APP`.`user`(`userId`), `tutorVote` int, `uploadDate` date not null )ENGINE=INNODB;
