f4b6582703dc8f9d83b7fe5032c7be29d25759b7
7Vanessa <vmohammedi@outlook.fr>
2024-01-16 18:08:31 +0100

    issue corrected on the display of the date picker

--- Front-end/src/components/DefenseDateLocation/DefenseDateLocation.js
+++ Front-end/src/components/DefenseDateLocation/DefenseDateLocation.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './DefenseDateLocation.css';
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DefenseDateLocation = ({dateId, locationId}) => {
     const [startDate, setStartDate] = useState(new Date());
@@ -17,7 +18,12 @@ const DefenseDateLocation = ({dateId, locationId}) => {
                         "font-size": "20px"
                     }}>Select a Date</p>
                     <div className="box" style={{"background-color": "#A3A3A3"}}>
-                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
+                        <DatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            dateFormat="dd/MM/yyyy"
+                        />
+
                     </div>
                 </div>
             </div>
