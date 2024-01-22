ce8bf56f2ac0c6fc6f323df2eb695f090423cad0
7Vanessa <vmohammedi@outlook.fr>
2024-01-22 16:31:04 +0100

    header improvement of the styles

--- Front-end/src/components/Header/Header.js
+++ Front-end/src/components/Header/Header.js
@@ -1,26 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Header.css';
-import logo from '../../assets/logo-efrei.svg'
-import {Link} from "react-router-dom";
+
+import logo from '../../assets/logo-efrei.svg';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="logo-container">
                 <Link to="/">
-                    <img src={logo} className="logo" alt="logo" />
+                    <img src={logo} className="logo" alt="Logo" />
                 </Link>
             </div>
+
             <div className="menu">
                 <Link to="/generate-a-date">
-                    <div>
-                        Plan Defense
-                    </div>
+                    <div>Plan Defense</div>
                 </Link>
+
                 <Link to="/reports">
-                    <div>
-                        Reports
-                    </div>
+                    <div>Reports</div>
                 </Link>
             </div>
         </header>
